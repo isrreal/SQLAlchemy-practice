d51764f27d4ec16ea50eeb6281b2e1944836c299
leggen-assis <souzaferreira437@gmail.com>
2025-08-22 11:21:27 -0300

    feat: delete entities file added

---
+++ delete_main.py
@@ -0,0 +1,28 @@
+from typing import Optional
+from conf.db_session import create_session
+from models.revendedor import Revendedor
+from models.picole import Picole
+
+def deletar_picole(id_picole: int) -> None:
+    with create_session() as session:
+        picole: Picole = session.query(Picole).filter(Picole.id == id_picole).one_or_none()
+        
+        if picole:
+            session.delete(picole)
+            session.commit()
+        else:
+            print(f"O picolé com ID {id_picole} não foi encontrado.")
+
+def deletar_revendedor(id_revendedor: int) -> None:
+    with create_session() as session:
+        revendedor: Optional[Revendedor] = session.query(revendedor).filter(revendedor.id == id_revendedor).one_or_none()
+        
+        if revendedor:
+            session.delete(revendedor)
+            session.commit()
+        else:
+            print(f"O picolé com ID {id_revendedor} não foi encontrado.")
+
+if __name__ == "__main__":
+    deletar_picole(2)
+    deletar_revendedor(2)
